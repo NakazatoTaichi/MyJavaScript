b217f39b174801ec11fe75b1fea1b56b15c4c12b
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-02-11 04:05:14 +0900

    jsテンプレートリテラル

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 'use strict';
 
 /*
-javascriptにおける数値の表現
+javascriptにおける文字列の表現
 */
 
-//文字列
-console.log('It is a pen.');
-console.log('It\'s is a pen.');
-console.log("It is a pen.");
-console.log("It's \na \npen.");  //改行
+const fname = 'Taro';
+const lname = 'Yamada';
+
+//My name is Taro Yamada ,call me Taro!
+
+// console.log('My name is ' + fname + ' ' + lname + ', call me ' + fname + '!');
+console.log(`My name is ${fname} ${lname} ,call me ${fname}!`);
 
