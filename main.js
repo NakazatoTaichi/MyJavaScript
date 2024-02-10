9ffa725dd9393366547a265dba357b62c4be0db4
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-02-10 12:16:10 +0900

    jsの計算

--- main.js
+++ main.js
@@ -4,10 +4,11 @@
 javascriptにおける数値の表現
 */
 
-console.log(100);  //コメントアウト
-console.log(-10);
-console.log(2.5);
+console.log(10 - 3);
+console.log(10 + 3);
+console.log(10 * 3);
+console.log(10 ** 3);
+console.log(10 / 3);
+console.log(10 % 3);
 
-
-console.log(1.2e4);
-console.log(1.2e-4);
+console.log((10 - 3) * 3);
