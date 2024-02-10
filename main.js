172167b41d1a0d7dabd5ddd024b388c2ac8c2866
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-02-11 03:15:48 +0900

    jsの文字列

--- main.js
+++ main.js
@@ -4,24 +4,9 @@
 javascriptにおける数値の表現
 */
 
-//変数
-let price;
-//定数
-const rate = 1.1;
-
-price = 150;
-console.log(price * 120 * rate);
-console.log(price * 130 * rate);
-console.log(price * 140 * rate);
-
-// price = price ++;
-// console.log(price * 120 * rate);
-// console.log(price * 130 * rate);
-// console.log(price * 140 * rate);
-
-price += 1;
-console.log(price * 120 * rate);
-console.log(price * 130 * rate);
-console.log(price * 140 * rate);
-
+//文字列
+console.log('It is a pen.');
+console.log('It\'s is a pen.');
+console.log("It is a pen.");
+console.log("It's \na \npen.");  //改行
 
