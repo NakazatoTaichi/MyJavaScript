4ace876b78a7fa360ca77ef1811ccfae11f5fe70
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-02-13 01:29:42 +0900

    jsのswitch

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 'use strict';
 
-const score = Number(prompt('Score?'));
+// const score = Number(prompt('Score?'));
 
-if(score >= 90) {
-    console.log('A!');
-} else if (score >= 70) {
-    console.log('B!');
-} else {
-    console.log('C!');
+// if(score >= 90) {
+//     console.log('A!');
+// } else if (score >= 70) {
+//     console.log('B!');
+// } else {
+//     console.log('C!');
+// }
+
+const color = prompt('Color?');
+
+switch (color) {
+    case 'red':
+        console.log('Stop!');
+        break;
+    case 'yellow':
+        console.log('Slow down!');
+        break;
+    case 'blue':
+        console.log('Go!');
+        break;
+    default:
+        console.log('Wrong color!');
+        break;
 }
-
