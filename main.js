fff1cc258e32815f753166e2f40c4613eb1193af
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-02-13 01:52:41 +0900

    js反復処理

--- main.js
+++ main.js
@@ -1,28 +1,6 @@
 'use strict';
 
-// const score = Number(prompt('Score?'));
 
-// if(score >= 90) {
-//     console.log('A!');
-// } else if (score >= 70) {
-//     console.log('B!');
-// } else {
-//     console.log('C!');
-// }
-
-const color = prompt('Color?');
-
-switch (color) {
-    case 'red':
-        console.log('Stop!');
-        break;
-    case 'yellow':
-        console.log('Slow down!');
-        break;
-    case 'blue':
-        console.log('Go!');
-        break;
-    default:
-        console.log('Wrong color!');
-        break;
+for (let i = 0; i < 3; i++) {
+    console.log('Hello');
 }
