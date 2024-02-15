b2744978f070bd6c656f9f0be6c7788e3daefef7
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-02-15 19:49:03 +0900

    条件演算子

--- main.js
+++ main.js
@@ -1,25 +1,5 @@
 'use strict';
 
-// let command;
-
-// do {
-//     command = Number(prompt('Menu 1, 2, 3 or 0 to exit'))
-//     if (command === 0) {
-//         console.log('Exited');
-//     } else {
-//         console.log(`Menu ${command} processed. `);
-//     }
-// } while (command !== 0);
-
-let amount = 100;
-
-for (let year = 1; year <= 30; year++) {
-    amount *= 1.05;
-    if (amount < 200) {
-        continue;
-    }
-    if (amount > 300) {
-        break;
-    }
-    console.log(`Year ${year}: ${amount}`);
-}
+const score = Number(prompt('Score?'));
+const result = score > 80 ? 'A' : 'B';
+console.log(result);
