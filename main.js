6f8152fd9a4f1eac8a1e662e1e71fd0155246e87
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-02-18 00:32:32 +0900

    アロー関数式

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 'use strict';
 {
-    console.log(double(10));
-
     //関数宣言
-    function double(num) {
-        return num * 2;
-    }
+    // function double(num) {
+    //     return num * 2;
+    // }
 
     //関数式
     // const double = function (num) {
     //     return num * 2;
     // };
+
+    // アロー関数式
+    //引数がひとつの場合
+    // const double = num => {
+    //     return num * 2;
+    // };
+
+    //returnで返されるのが1行だった場合
+    const double = (num) => num * 2;
+
+    console.log(double(10));
+
 }
