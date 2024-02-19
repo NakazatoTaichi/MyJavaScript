285b2bb062cb70f53413e97084db1a79ab1c5a19
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-02-20 04:29:08 +0900

    配列forEach処理

--- main.js
+++ main.js
@@ -9,7 +9,11 @@
 
     scores.push(77, 85);
 
-    for (let i = 0; i < scores.length; i++) {
-        console.log(scores[i]);
-    }
+    // for (let i = 0; i < scores.length; i++) {
+    //     console.log(scores[i]);
+    // }
+
+    scores.forEach((score, index) => {
+        console.log(`${index}: ${score}`);
+    });
 }
