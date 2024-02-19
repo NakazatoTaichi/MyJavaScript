e9ab049527fa2ceaac6e3efb26eb21f9d51ade5c
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-02-20 04:17:30 +0900

    配列の中身をfor文で表示する

--- main.js
+++ main.js
@@ -7,8 +7,9 @@
         85,
     ];
 
-    console.log(scores[2]);
-    console.log(scores.length);
-    console.log(scores);
+    scores.push(77, 85);
 
+    for (let i = 0; i < scores.length; i++) {
+        console.log(scores[i]);
+    }
 }
