3fc3152dd2696f1bbaffd043c529f90c27cc071a
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-02-22 23:53:18 +0900

    コメントアウト外した

--- main.js
+++ main.js
@@ -5,9 +5,9 @@
         english: 90,
     };
 
-    // console.log(scores['english']);
-    console.log(scores.english);
-    // scores['math'] = 88;
-    scores.math = 88;
+    console.log(scores['english']);
+    // console.log(scores.english);
+    scores['math'] = 88;
+    // scores.math = 88;
     console.log(scores);
 }
