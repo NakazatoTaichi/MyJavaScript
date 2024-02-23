66ab137164b935e5458fa4bd27919904f84b70bc
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-02-24 05:54:36 +0900

    オブジェクトの計算

--- main.js
+++ main.js
@@ -5,12 +5,19 @@
         english: 90,
     };
 
+    let sum = 0;
+
+    scores.physics = 70;
+
     const entries = Object.entries(scores);
 
     // console.log(entries);
 
     entries.forEach((prop) => {
         // console.log(prop)
+        sum += prop[1];
         console.log(`${prop[0]}: ${prop[1]}`);
     });
+    console.log(`Sum: ${sum}`);
+    console.log(`Average: ${sum / entries.length}`);
 }
