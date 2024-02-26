0d471e8609c73677de37d9191eec46f70cbc9ecf
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-02-26 15:26:18 +0900

    スプレット構文

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict';
 {
-    const scores = [70, 90, 80, 85];
+    const moreScores = [77, 88];
+    const scores = [70, 90, 80, 85, ...moreScores];
     const [first, ...others] = scores;
 
     console.log(first);
