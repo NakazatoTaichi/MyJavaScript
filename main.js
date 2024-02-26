13a81f3aed00e6f965a425fd5903039b20862b14
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-02-26 19:07:31 +0900

    textContentを使う

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 'use strict';
 {
     document.querySelector('button').addEventListener('click', () => {
-        console.log('Clicked');
+        // console.log('Clicked');
+        // document.querySelector('p').textContent = 'こんにちは';
+        document.querySelector('p').textContent = document.querySelector('button').textContent;
     });
+
+    // console.log('Hello');
 }
