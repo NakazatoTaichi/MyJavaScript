0358e96dd1a9c3667b4721c41d89100255ead04d
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-02-26 15:34:50 +0900

    オブジェクトでの分割代入・レスト構文・スプレット構文

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 'use strict';
 {
-    const moreScores = [77, 88];
-    const scores = [70, 90, 80, 85, ...moreScores];
-    const [first, ...others] = scores;
+    const moreScores = {
+        shiro: 77,
+        goro: 88,
+    };
 
-    console.log(first);
+    const scores = {
+        taro: 80,
+        jiro: 70,
+        saburo: 90,
+        ...moreScores,
+    };
+    // const {taro, jiro, saburo} = scores;
+    const {taro, ...others} = scores;
+
+
+    console.log(taro);
+    // console.log(jiro);
+    // console.log(saburo);
     console.log(others);
+
+
 }
