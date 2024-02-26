34ef609ae2e88d0fb063caa0b1964a4c3f42fd64
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-02-26 15:47:57 +0900

    変数と配列/オブジェクトの代入時の挙動

--- main.js
+++ main.js
@@ -1,24 +1,14 @@
 'use strict';
 {
-    const moreScores = {
-        shiro: 77,
-        goro: 88,
-    };
+    let num = 10;
+    const numBackup = num;
+    num = 99;
+    console.log(num);   //99
+    console.log(numBackup);     //10
 
-    const scores = {
-        taro: 80,
-        jiro: 70,
-        saburo: 90,
-        ...moreScores,
-    };
-    // const {taro, jiro, saburo} = scores;
-    const {taro, ...others} = scores;
-
-
-    console.log(taro);
-    // console.log(jiro);
-    // console.log(saburo);
-    console.log(others);
-
-
+    const nums = [10, 20, 30];
+    const numsBackup = nums;
+    nums[0] = 99;
+    console.log(nums);  //[99, 20, 30]
+    console.log(numsBackup);    //[10, 20, 30]
 }
