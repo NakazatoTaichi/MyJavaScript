fc303f65f890540fc90a75d3744af7bb38be1e62
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-02-26 19:51:13 +0900

    querySelectorAll()と反復処理

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 'use strict';
 {
     document.querySelector('button').addEventListener('click', () => {
-        // console.log(document.querySelector('p').classList.contains('pink-bg'));
-
-        // document.querySelector('p').classList.add('pink-bg');
-
-        // if(document.querySelector('p').classList.contains('pink-bg') === false) {
-        //     document.querySelector('p').classList.add('pink-bg');
-        // } else {
-        //     document.querySelector('p').classList.remove('pink-bg');
-        // }
-        document.querySelector('p').classList.toggle('pink-bg');
+        // document.querySelector('li').textContent = 'Changed!';
+        // document.querySelectorAll('li')[0].textContent = 'Changed!';
+        // document.querySelectorAll('li')[1].textContent = 'Changed!';
+        // document.querySelectorAll('li')[2].textContent = 'Changed!';
+        document.querySelectorAll('li').forEach((li) => {
+            li.textContent = 'Changed!';
+        });
     });
 }
