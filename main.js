930aa13417524bd62ba7352e9a3902fd6f01a194
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-02-26 18:58:12 +0900

    ボタンを使ったイベントリスナー

--- main.js
+++ main.js
@@ -1,18 +1,6 @@
 'use strict';
 {
-    // let num = 10;
-    // const numBackup = num;
-    // num = 99;
-    // console.log(num);   //99
-    // console.log(numBackup);     //10
-
-    const nums = [10, 20, 30];
-
-    // const numsBackup = nums;
-    // const numsBackup = [10, 20, 30];
-    const numsBackup = [...nums];
-
-    nums[0] = 99;
-    console.log(nums);  //[99, 20, 30]
-    console.log(numsBackup);    //[10, 20, 30]
+    document.querySelector('button').addEventListener('click', () => {
+        console.log('Clicked');
+    });
 }
