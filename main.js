a5f7cb2901707ea925830e1a618bf0035b5c0504
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-02-27 22:07:43 +0900

    ラジオボタンの値にアクセス

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 'use strict';
 {
     document.querySelector('button').addEventListener('click', () => {
-        alert(document.querySelector('select').value);
+        document.querySelectorAll('input').forEach((radio) => {
+            if(radio.checked === true) {
+                alert(radio.value);
+            }
+        });
     });
 }
