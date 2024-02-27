ae3438f166fc16f820e455c8b6af832ac0e0e5cf
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-02-27 20:53:53 +0900

    複雑なセレクター

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 'use strict';
 {
     document.querySelector('button').addEventListener('click', () => {
-        // document.querySelector('li').textContent = 'Changed!';
+        document.querySelector('#second').textContent = 'Changed!';
         // document.querySelectorAll('li')[0].textContent = 'Changed!';
         // document.querySelectorAll('li')[1].textContent = 'Changed!';
         // document.querySelectorAll('li')[2].textContent = 'Changed!';
-        document.querySelectorAll('li').forEach((li) => {
-            li.textContent = 'Changed!';
-        });
+        // document.querySelectorAll('.target').forEach((li) => {
+        //     li.textContent = 'Changed!';
+        // });
     });
 }
