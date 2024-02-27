2d92d010cc9da983f2a77df60996ce91eaa1e8f7
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-02-28 04:01:04 +0900

    ランダムな整数値の生成

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 'use strict';
 {
-    const n = Number(prompt('Count?'));
-    for (let i = 0; i < n; i++) {
-        if (i % 3 === 0) {
-            console.log(`---item ${i}---`);
-        } else {
-            console.log(`===item ${i}===`);
-        }
+    for (let i = 0; i < 10; i++) {
+        const n = Math.floor(Math.random() * 3);
+        console.log(`${i}: ${n}`);
     }
 
 }
